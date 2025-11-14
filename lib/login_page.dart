4365d2f4dce663d4ed1f7ad1023c310fe38d9841
NicuBalatici <nicu.marian28@yahoo.com>
2025-11-14 17:15:26 +0200

    Login page added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -47,6 +47,7 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                   ),
+
                   const SizedBox(height: 50),
 
                   // --- Email Text Field ---
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Login App',
       home: LoginPage(),
+
     );
   }
 }
